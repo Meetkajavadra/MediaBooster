4729d7fe4f949edf2b306c45dc3cb80176e8562a
Meetkajavadra <Meet>
2022-10-14 18:00:45 +0530

    gallary app

---
+++ lib/global.dart
@@ -0,0 +1,83 @@
+class Global{
+
+  static List allImage = [
+    {
+      "type" : "Nature",
+      "imagetype" : "assets/image/main_nature.jpg",
+      "typeofimages" : [
+        {
+          "imagee" : "assets/image/mountain.jpg"
+        },
+        {
+          "imagee" : "assets/image/rain.jpg"
+        },
+        {
+          "imagee" : "assets/image/sea.jpg"
+        },
+        {
+          "imagee" : "assets/image/snowfall.jpg"
+        },
+        {
+          "imagee" : "assets/image/sunset.jpg"
+        }
+      ]
+    },
+    {
+      "type" : "Textures & Patterns",
+      "imagetype" : "assets/image/main_tandp.jpg",
+      "typeofimages" : [
+        {
+          "imagee" : "assets/image/bluetexture.jpg"
+        },
+        {
+          "imagee" : "assets/image/galaxy.jpg"
+        },
+        {
+          "imagee" : "assets/image/hand.jpg"
+        },
+        {
+          "imagee" : "assets/image/sand.jpg"
+        }
+      ]
+    },
+    {
+      "type" : "Cars",
+      "imagetype" : "assets/image/main_car.jpg",
+      "typeofimages" : [
+        {
+          "imagee" : "assets/image/lambo.jpg"
+        },
+        {
+          "imagee" : "assets/image/ferreri.jpg"
+        },
+        {
+          "imagee" : "assets/image/mustang.jpg"
+        },
+        {
+          "imagee" : "assets/image/srt.jpg"
+        },
+        {
+          "imagee" : "assets/image/amg.jpg"
+        }
+      ]
+    },
+    {
+      "type" : "Bikes",
+      "imagetype" : "assets/image/main_bike.jpg",
+      "typeofimages" : [
+        {
+          "imagee" : "assets/image/ducati.jpg"
+        },
+        {
+          "imagee" : "assets/image/ninjah1.jpg"
+        },
+        {
+          "imagee" : "assets/image/harley.jpg"
+        },
+        {
+          "imagee" : "assets/image/ninjah2.jpg"
+        }
+      ]
+    }
+  ];
+}
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:carousel_slider_app/allimage.dart';
+import 'package:carousel_slider_app/homepage.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: 'homepage',
+      routes: {
+        'homepage': (context) => const HomePage(),
+        'allimage': (context) => const All_image(),
+      },
+    );
+  }
+}
